a10997bdef48622960d3860da06103cc7294b272
omy86 <harrysytito@gmail.com>
2020-03-20 08:25:07 +0100

    add path about

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
-    console.log("serveur started...")
+    console.log("serveur started...");
+});
+
+app.get('/', (req, res) => {
+    console.log('access to / path');
+    res.send('hola amigos !');
+});
+
+app.get("/about", (req, res) => {
+    res.send("about");
 });
